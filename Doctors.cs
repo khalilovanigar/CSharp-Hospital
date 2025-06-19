cb10220218d2759751f57ec0b78e4f6b6fe01fe3
khalilovanigar <khalilovanigar64@gmail.com>
2025-06-19 17:52:29 +0400

    Update Doctors.cs

--- Doctors.cs
+++ Doctors.cs
@@ -1,16 +1,29 @@
 using System;
-namespace DoctorsNamespace;
+using System.Collections.Generic;
+using ReservNamespace;
 
-public class Doctor
+namespace DoctorsNamespace
+
 {
-    public string Name { get; set; }
-    public string Surname { get; set; }
-    public int Experience { get; set; }
-
-    public Doctor(string name, string surname, int experience)
+    public class Doctor
     {
-        Name = name;
-        Surname = surname;
-        Experience = experience;
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Experience { get; set; }
+        public List<Reserv> Reserved { get; set; }
+
+
+        public Doctor(string name, string surname, int experience)
+        {
+            Name = name;
+            Surname = surname;
+            Experience = experience;
+            Reserved = new List<Reserv>
+    {
+        new Reserv("09:00-11:00"),
+        new Reserv("12:00-14:00"),
+        new Reserv("15:00-17:00")
+    };
+        }
     }
 }
