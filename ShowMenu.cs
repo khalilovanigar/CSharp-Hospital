4a66ac880a4cd5608d6fe9981e4981fd80e03dbe
khalilovanigar <khalilovanigar64@gmail.com>
2025-06-19 17:52:50 +0400

    Update ShowMenu.cs

--- ShowMenu.cs
+++ ShowMenu.cs
@@ -3,27 +3,9 @@ using System.Collections.Generic;
 
 namespace ShowMenuNamespace
 {
-    class Program
+    public static class ShowMenuHelper
     {
-        static void Main()
-        {
-            var departments = new List<string> { "Pediatriya", "Travmatologiya", "Stomatologiya" };
-            int depIndex = ShowMenu(departments, "Choose Department:");
-
-            var doctors = new List<string> { "Sevda Ehmedova", "Selim Eliyev", "Leyla Memmedli" };
-            int docIndex = ShowMenu(doctors, "Choose Doctor:");
-
-            var hours = new List<string> { "09:00-11:00", "12:00-14:00", "15:00-17:00" };
-            int hourIndex = ShowMenu(hours, "Choose an hour:");
-            System.Console.WriteLine();
-
-            System.Console.WriteLine($"You choose:");
-            System.Console.WriteLine($"Department: {departments[depIndex]}");
-            System.Console.WriteLine($"Doctor: {doctors[docIndex]}");
-            System.Console.WriteLine($"Hour: {hours[hourIndex]}");
-        }
-
-        static int ShowMenu(List<string> options, string title)
+        public static int ShowMenu(List<string> options, string title)
         {
             int selectedIndex = 0;
             ConsoleKey key;
@@ -31,20 +13,20 @@ namespace ShowMenuNamespace
             do
             {
                 Console.Clear();
-                System.Console.WriteLine(title);
-                System.Console.WriteLine(new string('-', title.Length));
+                Console.WriteLine(title);
+                Console.WriteLine(new string('-', title.Length));
 
                 for (int i = 0; i < options.Count; i++)
                 {
                     if (i == selectedIndex)
                     {
                         Console.ForegroundColor = ConsoleColor.Green;
-                        System.Console.WriteLine($"> {options[i]}");
+                        Console.WriteLine($"> {options[i]}");
                         Console.ResetColor();
                     }
                     else
                     {
-                        System.Console.WriteLine($"  {options[i]}");
+                        Console.WriteLine($"  {options[i]}");
                     }
                 }
 
